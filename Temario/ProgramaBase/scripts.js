1496d80731679508dd1a62e81ab29135bd1fa967
Paula <paulalopezh@hotmail.com>
2023-02-26 18:15:44 +0100

    comit inicial curso js

---
+++ Temario/ProgramaBase/scripts.js
@@ -0,0 +1,4 @@
+function miFuncion(){
+    let mensaje = document.getElementById("cajaTexto").value;//Recoger valor de un input text
+    document.getElementById("mostrarDatos").innerHTML = mensaje;//Dar valor por ejemplo a un párrafo
+}
